444e8a8f33fd03b75d275bdef2caf9ae75575bc3
Ashwini <salunkheashwini1998@gmail.com>
2022-12-26 17:04:44 +0530

    Greeting Massage

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿    namespace EmployeeWageProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problem");
+        }
+    }
+}
